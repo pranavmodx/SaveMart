e4773cdc3653073a890d65d71131f516f687b6f5
aryajayadevkm <aryajayadevkm@gmail.com>
2020-11-18 22:39:29 +0530

    added Shops model, bulk update script and data

---
+++ backend/bulk_add.py
@@ -0,0 +1,29 @@
+import psycopg2
+
+db = psycopg2.connect(
+    host='localhost',
+    user='admin',
+    password='admin123',
+    database='savemart',
+    port=5432
+)
+cursor = db.cursor()
+
+
+def add_shop_data(file):
+    with open(file, 'r') as f:
+        try:
+            cmd = f"""
+                        COPY "Shops" (shop_name, longitude, latitude, address) 
+                        FROM STDIN WITH (FORMAT CSV, HEADER TRUE);
+                    """
+            cursor.copy_expert(cmd, f)
+            db.commit()
+        except Exception as e:
+            print(f"Error due to {e}")
+            db.rollback()
+
+
+if __name__ == "__main__":
+    add_shop_data('../data/shop_data.csv')
+
