2ca399d1b656ea7001cecf95f8bb64bb7078d653
aryajayadevkm <aryajayadevkm@gmail.com>
2020-11-28 10:59:01 +0530

    added __str__

--- savemart/models.py
+++ savemart/models.py
@@ -9,12 +9,19 @@ class Shop(models.Model):
         max_digits=12, decimal_places=0, null=True, blank=True)
     location = models.PointField(null=True, blank=True)
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class ProductShop(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
