cd0c5c0aab55efb0efed673ccb1eaf1e885cd5dc
Pranav Shridhar <pranavmodx@gmail.com>
2020-12-07 16:28:27 +0530

    add cors allow

--- backend/settings.py
+++ backend/settings.py
@@ -43,10 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.gis',
     'rest_framework',
     'django_extensions',
+    'corsheaders',
     'savemart',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,3 +131,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOW_ALL_ORIGINS = True
