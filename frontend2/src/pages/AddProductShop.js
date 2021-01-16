fd4286c6b594947d7afe1f3c5e60a718df810584
Pranav Shridhar <pranavmodx@gmail.com>
2021-01-16 18:21:01 +0530

    add add productshop

--- frontend2/src/pages/AddProductShop.js
+++ frontend2/src/pages/AddProductShop.js
@@ -1,8 +1,10 @@
 import React from "react";
-
+import SelectSearch from 'react-select-search';
 import axios from "axios";
 
 const AddProductShop = () => {
+  const [shops, setShops] = useState([])
+  const [products, setProducts] = useState([])
   const handleAddProductShop = () => {
     axios
       .post("http://localhost:8000/product_shop/product/", {
@@ -15,8 +17,26 @@ const AddProductShop = () => {
       });
   };
 
+  useEffect(() => {
+    axios.get("http://localhost:8000/shops/").then((res) => {
+      console.log(res.data);
+      setShops(res.data);
+    });
+
+    axios.get("http://localhost:8000/products/").then((res) => {
+      console.log(res.data);
+      setProducts(res.data);
+    });
+  }, [])
+
   return (
     <div>
+      <SelectSearch
+        options={countries}
+        multiple
+        search
+        placeholder="Select your country"
+    />
       <button onClick={handleAddProductShop}>Add Product Shop</button>
     </div>
   );
